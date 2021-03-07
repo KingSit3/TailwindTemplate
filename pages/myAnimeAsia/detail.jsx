2e9a641c6cd0a027167e249d47caea00c5c470e4
KingSit3 <i.ardiansyah93@gmail.com>
2021-03-07 16:59:34 +0700

    Done Detail MyAnimeAsia

--- pages/myAnimeAsia/detail.jsx
+++ pages/myAnimeAsia/detail.jsx
@@ -13,31 +13,192 @@ const Homepage = () => {
                 </div>
             {/* {{-- End Navbar --}} */}
 
-            {/* {{-- On Progress Section --}} */}
+            {/* {{-- Top Section --}} */}
                 <div className="my-5 flex flex-row mx-32">
-                    <img className="w-64" src="../image/otaku-anime.jpg" alt="Poster"/>
+                    <img src="../image/otaku-anime.jpg" alt="Poster"/>
                     <div className="ml-10">
                         <p className="font-bold text-2xl">Tatoeba Last Dungeon Mae no Mura no Shounen ga Joban no Machi de Kurasu Youna Monogatari</p>
-                        <div className="flex flex-row space-x-4">
-                            <p>Japanese:</p>
-                            <p>俺だけ入れる隠しダンジョン</p>
+                        <table className="table mt-3">
+                            <tr>
+                                <td>Japanese</td>
+                                <td>:</td>
+                                <td>俺だけ入れる隠しダンジョン</td>
+                            </tr>
+                            <tr>
+                                <td>Episodes</td>
+                                <td>:</td>
+                                <td>24</td>
+                            </tr>
+                            <tr>
+                                <td>Premiered</td>
+                                <td>:</td>
+                                <td>Spring 2021</td>
+                            </tr>
+                            <tr>
+                                <td>Producers</td>
+                                <td>:</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>Licensors</td>
+                                <td>:</td>
+                                <td>-</td>
+                            </tr>
+                            <tr>
+                                <td>Status</td>
+                                <td>:</td>
+                                <td>Not yet aired</td>
+                            </tr>
+                            <tr>
+                                <td>Studios</td>
+                                <td>:</td>
+                                <td>Doga Kobo</td>
+                            </tr>
+                            <tr>
+                                <td>Source</td>
+                                <td>:</td>
+                                <td>Light novel</td>
+                            </tr>
+                            <tr>
+                                <td>Genres</td>
+                                <td>:</td>
+                                <td>Harem, Comedy, Romance, School</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            {/* {{-- End Top Section --}} */}
+
+            {/* Episodes Links */}
+                <div className="border-t-2 border-trueGray-700">
+                    {/* EPisode Link */}
+                    <div className="px-40 mt-3 font-semibold">
+                        <div className="bg-trueGray-900 p-5 rounded-2xl">
+                            <span className="bg-blue-700 px-16 rounded-md text-xl">Episode 01</span>
+                            <div className="flex justify-between">
+                            <div className="flex flex-col justify-center">
+                                <p className="text-center">Streaming</p>
+                                <div className="grid grid-cols-2 gap-4 text-center mt-2">
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English 360p</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia 360p</a>
+                                </div>
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <p className="text-center">Download Mp4</p>
+                                <div className="grid grid-cols-2 gap-4 text-center mt-2">
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">720p</a>
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">360p</a>
+                                </div>
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <p className="text-center">Download Mkv</p>
+                                <div className="grid grid-cols-2 gap-4 text-center mt-2">
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">720p</a>
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">360p</a>
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex flex-row space-x-4">
-                            <p>Episode:</p>
-                            <p>24</p>
-                        </div>
-                        <div className="flex flex-row space-x-4">
-                            <p>Japanese:</p>
-                            <p>俺だけ入れる隠しダンジョン</p>
                         </div>
                     </div>
-
-
+                    {/* End Episode Link */}
+                    {/* EPisode Link */}
+                    <div className="px-40 mt-3 font-semibold">
+                        <div className="bg-trueGray-900 p-5 rounded-2xl">
+                            <span className="bg-blue-700 px-16 rounded-md text-xl">Episode 02</span>
+                            <div className="flex justify-between">
+                            <div className="flex flex-col justify-center">
+                                <p className="text-center">Streaming</p>
+                                <div className="grid grid-cols-2 gap-4 text-center mt-2">
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English 360p</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia 360p</a>
+                                </div>
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <p className="text-center">Download Mp4</p>
+                                <div className="grid grid-cols-2 gap-4 text-center mt-2">
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">720p</a>
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">360p</a>
+                                </div>
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <p className="text-center">Download Mkv</p>
+                                <div className="grid grid-cols-2 gap-4 text-center mt-2">
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">720p</a>
+                                    <a className="p-3 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">360p</a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    {/* End Episode Link */}
                 </div>
-            {/* {{-- End On Progress Section --}} */}
+            {/* End Episodes Links */}
+
+            {/* Episodes Links V2 */}
+                <div className="border-t-2 mt-5 border-trueGray-700">
+                    {/* EPisode Link */}
+                    <div className="px-40 mt-3 font-semibold">
+                        <div className="bg-trueGray-900 p-5 rounded-2xl">
+                            <span className="bg-blue-700 px-16 rounded-md text-xl">Episode 01</span>
+                            <div className="flex space-x-6 items-center mt-4">
+                                <p className="text-center">Streaming: </p>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English 360p</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia 360p</a>
+                            </div>
+                            <div className="flex space-x-6 items-center mt-4">
+                                <p className="text-center">Download Mp4: </p>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English 360p</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia 360p</a>
+                            </div>
+                            <div className="flex space-x-6 items-center mt-4">
+                                <p className="text-center">Download Mkv: </p>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Server 1</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Server 2</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Server 3 [360p]</a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* End Episode Link */}
+                    {/* EPisode Link */}
+                    <div className="px-40 mt-3 font-semibold">
+                        <div className="bg-trueGray-900 p-5 rounded-2xl">
+                            <span className="bg-blue-700 px-16 rounded-md text-xl">Episode 02</span>
+                            <div className="flex space-x-6 items-center mt-4">
+                                <p className="text-center">Streaming: </p>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English 360p</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia 360p</a>
+                            </div>
+                            <div className="flex space-x-6 items-center mt-4">
+                                <p className="text-center">Download Mp4: </p>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">English 360p</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Indonesia 360p</a>
+                            </div>
+                            <div className="flex space-x-6 items-center mt-4">
+                                <p className="text-center">Download Mkv: </p>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Server 1</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Server 2</a>
+                                    <a className="p-1.5 rounded-lg bg-blue-700 hover:bg-blue-800 duration-200" href="#">Server 3 [360p]</a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* End Episode Link */}
+                </div>
+            {/* End Episodes Links */}
 
             {/* {{-- Footer --}} */}
-                <div className="flex justify-center px-10 pt-2 bg-black shadow-2xl">
+                <div className="flex justify-center mt-5 px-10 pt-2 bg-black shadow-2xl">
                     <div className="flex flex-col items-center justify-center ">
                         <span className="font-bold text-lg mb-3 cursor-default">Partners</span>
                         <div className="flex space-x-4">
@@ -61,7 +222,6 @@ const Homepage = () => {
                 
             {/* {{-- End Footer --}} */}
         </div>
-
 
     </div>
     )

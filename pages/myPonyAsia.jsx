c6cf092e484c3e7ea462f9ed99f0459586b3c97c
KingSit3 <i.ardiansyah93@gmail.com>
2021-03-13 23:27:46 +0700

    MPA Responsive update

--- pages/myPonyAsia.jsx
+++ pages/myPonyAsia.jsx
@@ -2,20 +2,26 @@ const Homepage = () => {
     return (
     <div>
   
-        <div className="bg-trueGray-800 text-white w-full h-screen">
+        <div className="bg-trueGray-800 text-trueGray-100 w-full h-screen">
             {/* Logo */}
-                <div className="mx-20 md:mx-40 lg:mx-60 xl:mx-96 pt-2 lg:pt-10">
+                <div className="mx-14 sm:mx-20 md:mx-40 lg:mx-60 xl:mx-96 pt-7 lg:pt-10">
                     <img src="/image/mpa.png" alt="MPA Logo"/>
                 </div>
             
             {/* Content */}
-                <div className="flex justify-between flex-col items-center lg:flex-row space-y-10 lg:space-y-0 space-x-0 lg:space-x-16 px-14 lg:px-24 mt-10 lg:mt-10 text-center text-2xl font-bold">
+                <div className="flex justify-between items-center text-center font-bold flex-col lg:flex-row 
+                        space-y-7 mt-7 space-x-0 px-14
+                        sm:px-28 sm:text-2xl
+                        md:text-3xl
+                        lg:space-y-0 lg:space-x-16 lg:px-24 lg:mt-10 lg:text-4xl">
                     <a href="#">
-                        <img className="rounded-xl hover:opacity-50 duration-200 shadow-xl transform hover:rotate-3" src="/image/img_landscape.png" alt="Thumbnail"/>
+                        {/* <span className="absolute ml-20 mt-20 text-3xl opacity-0 group-hover:opacity-100 duration-200"> My Little Pony </span> */}
+                        <img className="rounded-xl hover:opacity-25 duration-200 shadow-xl transform hover:rotate-3" src="/image/img_landscape.png" alt="Thumbnail"/>
                         <span> My Little Pony </span>
                     </a>
                     <a href="#">
-                        <img className="rounded-xl hover:opacity-50 duration-200 shadow-xl transform hover:-rotate-3" src="/image/img_landscape2.jpg" alt="Thumbnail"/>
+                        {/* <span className="absolute ml-32 mt-20 text-3xl opacity-0 group-hover:opacity-100 duration-200"> Anime </span> */}
+                        <img className="rounded-xl hover:opacity-25 duration-200 shadow-xl transform hover:-rotate-3" src="/image/img_landscape2.jpg" alt="Thumbnail"/>
                         <span> Anime </span>
                     </a>
                 </div>

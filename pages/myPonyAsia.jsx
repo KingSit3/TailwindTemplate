4e2591368151a0ef2b3dd62a80c90b8461406997
KingSit3 <i.ardiansyah93@gmail.com>
2021-03-13 22:40:03 +0700

    MPA Add Responsive

--- pages/myPonyAsia.jsx
+++ pages/myPonyAsia.jsx
@@ -3,19 +3,22 @@ const Homepage = () => {
     <div>
   
         <div className="bg-trueGray-800 text-white w-full h-screen">
-            <div className="mx-96 pt-10">
-                <img src="/image/mpa.png" alt="MPA Logo"/>
-            </div>
-            <div className="flex justify-between space-x-16 px-24 mt-10 text-center text-2xl font-bold ">
-                <a href="#">
-                    <img width="1028" className="rounded-xl hover:opacity-50 duration-200 shadow-xl transform hover:rotate-3" src="/image/img_landscape.png" alt="Thumbnail"/>
-                    <span> My Little Pony </span>
-                </a>
-                <a href="#">
-                    <img width="1028" className="rounded-xl hover:opacity-50 duration-200 shadow-xl transform hover:-rotate-3" src="/image/episodethumbnail.png" alt="Thumbnail"/>
-                    <span> Anime </span>
-                </a>
-            </div>
+            {/* Logo */}
+                <div className="mx-20 md:mx-40 lg:mx-60 xl:mx-96 pt-2 lg:pt-10">
+                    <img src="/image/mpa.png" alt="MPA Logo"/>
+                </div>
+            
+            {/* Content */}
+                <div className="flex justify-between flex-col items-center lg:flex-row space-y-10 lg:space-y-0 space-x-0 lg:space-x-16 px-14 lg:px-24 mt-10 lg:mt-10 text-center text-2xl font-bold">
+                    <a href="#">
+                        <img className="rounded-xl hover:opacity-50 duration-200 shadow-xl transform hover:rotate-3" src="/image/img_landscape.png" alt="Thumbnail"/>
+                        <span> My Little Pony </span>
+                    </a>
+                    <a href="#">
+                        <img className="rounded-xl hover:opacity-50 duration-200 shadow-xl transform hover:-rotate-3" src="/image/img_landscape2.jpg" alt="Thumbnail"/>
+                        <span> Anime </span>
+                    </a>
+                </div>
         </div>
 
     </div>
